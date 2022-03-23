d82bd1f757a3088b8f79dcca51fec95baef2152c
[umid shamdinli] <[umidash@code.edu.az]>
2022-03-23 23:27:40 +0400

    Added Database-Lhiye

---
+++ BasketTask/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BasketTask.Data;
+using BasketTask.Models;
+using BasketTask.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            List<DesignCard> designCards = await _context.DesignCards.ToListAsync();
+            List<OwlCarusel> owlCarusels = await _context.OwlCarusels.ToListAsync();
+
+            HomeVM homeVM = new HomeVM
+            {
+                DesignCards=designCards,
+                OwlCarusels=owlCarusels
+            };
+
+
+            return View(homeVM);
+        }
+
+      
+    }
+}
---
+++ BasketTask/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BasketTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketTask.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<DesignCard> DesignCards { get; set; }
+        public DbSet<OwlCarusel> OwlCarusels { get; set; }
+
+
+    }
+}
